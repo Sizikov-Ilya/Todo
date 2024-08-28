0fdde0c895c07aaefa1184fca3ce0421a0bc5507
Ilya <sizikovilya01@gmail.com>
2024-08-28 13:04:44 +0300

    fix main.js and add .gitignore

--- js/main.js
+++ js/main.js
@@ -8,27 +8,29 @@ let tasks = [];
 const renderTask = (task) => {
   const cssClass = task.done ? "task-title task-title--done" : "task-title";
 
-  const taskHTML = `<li id="${task.id}" class="list-group-item d-flex justify-content-between task-item">
-      <span class="${cssClass}">${task.text}</span>
-      <div class="task-item__buttons">
-        <button type="button" data-action="done" class="btn-action">
-          <img src="./img/tick.svg" alt="Done" width="18" height="18">
-        </button>
+  const taskHTML = `
+  <li id="${task.id}" class="list-group-item d-flex justify-content-between task-item">
+    <span class="${cssClass}">${task.text}</span>
+    <div class="task-item__buttons">
+      <button type="button" data-action="done" class="btn-action">
+        <img src="./img/tick.svg" alt="Done" width="18" height="18">
+      </button>
       <button type="button" data-action="delete" class="btn-action">
         <img src="./img/cross.svg" alt="Done" width="18" height="18">
       </button>
-      </div>
-    </li>`;
+    </div>
+  </li>`;
 
   tasksList.insertAdjacentHTML("beforeend", taskHTML);
 };
 
 const checkEmptyList = () => {
   if (tasks.length === 0) {
-    const emptyListHTML = `<li id="emptyList" class="list-group-item empty-list">
-					<img src="./img/leaf.svg" alt="Empty" width="48" class="mt-3">
-					<div class="empty-list__title">Список дел пуст</div>
-				</li>`;
+    const emptyListHTML = `
+    <li id="emptyList" class="list-group-item empty-list">
+      <img src="./img/leaf.svg" alt="Empty" width="48" class="mt-3">
+      <div class="empty-list__title">Список дел пуст</div>
+    </li>`;
     tasksList.insertAdjacentHTML("afterbegin", emptyListHTML);
   }
 
